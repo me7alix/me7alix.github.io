9e5f44e88236736426644523aae5af1660edefdc
me7alix <me7alix@gmail.com>
2026-06-13 23:49:01 +0300

    make code more readable

--- src/main.c
+++ src/main.c
@@ -9,11 +9,10 @@
 #include "../3rdparty/htmpl.h"
 
 typedef struct {
+	int id;
 	char *title;
 	char *link;
 } Post;
-
-#include INDEX_TEMPLATE
 
 char *xstrdup(const char *s) {
     size_t n = strlen(s) + 1;
@@ -22,6 +21,7 @@ char *xstrdup(const char *s) {
     return copy;
 }
 
+/* ID is a 4 digits long integer */
 int extract_id(char *str) {
 	int res = 0;
 	int pwr = 1000;
@@ -34,51 +34,66 @@ int extract_id(char *str) {
 	return res;
 }
 
+/* index_tmpl(Post *posts, size_t count) */
+#include INDEX_TEMPLATE
+
 int main(void) {
 	Post posts[9999];
 	size_t count = 0;
 
-	DIR *dir;
-	struct dirent *ent;
-	if ((dir = opendir("./posts")) != NULL) {
-		while ((ent = readdir(dir)) != NULL) {
-			char *file = ent->d_name;
-			if (file[0] >= '0' && file[0] <= '9') {
-				char md[1024];
-				char html[1024];
-				char link[1024];
-				char cmd[1024];
-
-				sprintf(md, "./posts/%s", file);
-				sprintf(html, "./pages/%s.html", file);
-				sprintf(cmd,
-					 "./build/md2html '%s' '%s'",
-					 md, html);
-
-				posts[count++] = (Post){
-					.title = xstrdup(file),
-					.link = xstrdup(html),
-				};
-
-				printf("CMD: %s\n", cmd);
-				if (system(cmd) != 0) {
-					fprintf(stderr, "Error generating html\n");
-					return 1;
-				}
-			}
-		}
-		closedir(dir);
-	} else {
+	DIR *dir = opendir("./posts");
+	if (dir == NULL) {
 		fprintf(stderr, "Error opening directory\n");
 		return 1;
 	}
 
+	struct dirent *ent;
+	while ((ent = readdir(dir)) != NULL) {
+		char *file = ent->d_name;
+		size_t len = strlen(file);
+
+		/* Filter */
+		if (len < 7) continue;
+		int id = extract_id(file);
+		if (file[len-3] != '.' ||
+			file[len-2] != 'm' ||
+			file[len-1] != 'd' ||
+			id == -1
+		) continue;
+
+		char md[1024];
+		char html[1024];
+		char link[1024];
+		char cmd[1024];
+
+		sprintf(md, "./posts/%s", file);
+		sprintf(html, "./pages/%s.html", file);
+
+		const char *mdf = "./build/md2html '%s' '%s'";
+		sprintf(cmd, mdf, md, html);
+
+		char *name = xstrdup(file);
+		name[len-3] = '\0';
+		name += 5;
+
+		posts[count++] = (Post){
+			.id = id,
+			.title = name,
+			.link = xstrdup(html),
+		};
+
+		printf("CMD: %s\n", cmd);
+		if (system(cmd) != 0) {
+			return 1;
+		}
+	}
+	closedir(dir);
+
+	/* Sorting */
 	while (true) {
 		bool sorted = true;
 		for (size_t i = 0; i < count - 1; i++) {
-			int cid = extract_id(posts[i].title);
-			int nid = extract_id(posts[i+1].title);
-			if (cid < nid) {
+			if (posts[i].id < posts[i+1].id) {
 				Post tmp = posts[i];
 				posts[i] = posts[i+1];
 				posts[i+1] = tmp;
